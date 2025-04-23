227f7cb9495b842e90d80a5c2c16aa3e79cf7595
Subidit Chakraborty <161379651+subidit123@users.noreply.github.com>
2025-04-23 17:43:43 +0530

    Add files via upload

---
+++ simplequiz/to-do1/script.js
@@ -0,0 +1,16 @@
+function addTask(){
+    const newTask=document.createElement('li')
+    const taskList=document.getElementById('taskList')
+    taskList.appendChild(newTask)
+    newTask.textContent=document.getElementById('inputTask').value
+    document.getElementById('inputTask').value=""
+    deleteTask(newTask)
+}
+function deleteTask(newTask){
+    const deletebtn=document.createElement('button')
+    deletebtn.textContent="Delete"
+    newTask.appendChild(deletebtn)
+    deletebtn.onclick=function(){
+        newTask.remove()
+
+}}
